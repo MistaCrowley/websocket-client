43de7ed1907ad22961351a0cf721e7a904b617d4
MistaCrowley <g14n7.5qu1d@gmail.com>
2021-12-14 09:16:20 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const Html5WebSocket = require('html5-websocket');
+const reconnectingWebsocket = require('reconnecting-websocket');
+//initialize websockets
+
+let ws_host = 'localhost';
+let ws_port = '3000';
+
+const options = { WebSocket: Html5WebSocket };
+//const rws = new ReconnectingWebSocket('ws://localhost:3000/ws', undefined, op>
+const rws = new ReconnectingWebSocket('ws://' + ws_host + ':' + ws_port + '/ws'>
+
+
+rws.timeout = 1000;
+
+rws.addEventListener('open', () => {
+        console.log('[Client] Connection to websocket server was opened');
+
+        rws.send('helllooooo this is a message from a clienteleee');
+
+        rws.send(JSON.stringify({
+                method: 'set-background-color',
+                params: {
+                        color: 'blue'
+                }
+        }));
+
+});
+
+rws.addEventListener('message', (e) => {
+        console.log('[Client] Message received: ' + e.data);
+
+        try {
+                let m = JSON.parse(e.data);
+                handleMessage(m);
+        } catch (err) {
+                console.log('[Client] Message is not parseable to JSON.');
+        }
+
+});
+
+rws.addEventListener('close', () => {
+        console.log('[Client] connection closed');
+});
+
+rws.onerror = (err) => {
+        if (err.code == 'EHOSTDOWN') {
+                console.log('[Client] Error: server down.');
+        }
+};
+
+
+//message handlers
+//functions that can handles specific messages
+
+let handlers = {
+        "set-background-color" : function(m) {
+                console.log('[Client] set-background-color handler.');
+                console.log('[Client] Color is ' + m.params.color);
+        }
+};
+
+
+function handleMessage(m) {
+
+        if (m.method == undefined) {
+                return;
+        }
+        let method = m.method;
+        if (method) {
+                if (handlers[method]) {
+                        let handler = handlers[method];
+                        handler(m);
+                } else {
+                    console.log('[Client] No handler defined for method' + method + '.');
+                }
+        }
+}
